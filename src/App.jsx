6d8c4e0f63ce06560a4408b54a8c0aa86763ed30
myfruitmango <ctrl.fruitman90@gmail.com>
2023-12-15 13:39:25 +0700

    create: new app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+      <span>Hello World</span>
+    </div>
+  );
+};
+
+export default App;
