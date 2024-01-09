e6cb58bdfecd53a82c1e45318b20292f040598ca
fadjaraidil <m.fadjar@uni.net.id>
2024-01-09 08:27:12 +0700

    update: template

--- src/pages/home/template.jsx
+++ src/pages/home/template.jsx
@@ -1,7 +1,15 @@
-const Template = () => {
+import Descriptions from '../../components/elements/descriptions'
+
+const Template = ({ type, port }) => {
 	return (
 		<div className="h-screen w-screen flex justify-center items-center">
-			<h6>Hello World</h6>
+			<div className="grid gap-4 mb-6">
+				<h4 className="text-center">Hello World</h4>
+				<div className="grid gap-2">
+					<Descriptions label="Type" value={type} />
+					<Descriptions label="Port" value={port} />
+				</div>
+			</div>
 		</div>
 	)
 }
