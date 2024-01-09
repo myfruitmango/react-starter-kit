56e9ae2ef07e680d258707d27314575d78d7c86c
fadjaraidil <m.fadjar@uni.net.id>
2024-01-09 08:26:42 +0700

    create: api-instance

---
+++ src/config/api/api-instance.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+import { locStorage } from '../utils/session-local-storage'
+
+const apiInstance = axios.create({
+	baseURL: process.env.REACT_APP_API_SERVER_URL,
+	timeout: 6000
+})
+
+apiInstance.interceptors.request.use(
+	(config) => {
+		const authToken = locStorage.getItem('authToken').access_token
+		if (authToken) {
+			config.headers.Authorization = `Bearer ${authToken}`
+		}
+
+		return config
+	},
+	(error) => {
+		return Promise.reject(error)
+	}
+)
+
+export default apiInstance
