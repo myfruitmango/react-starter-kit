bc1977be7d1ccf9abc4ab8f23e557572a50be6e0
fadjaraidil <m.fadjar@uni.net.id>
2024-01-09 08:28:29 +0700

    update: pages

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,9 @@
 import Template from './template'
 
+const type = process.env.REACT_APP_ENV
+const port = process.env.PORT
 const HomePage = () => {
-	return <Template />
+	return <Template type={type} port={port} />
 }
 
 export default HomePage
