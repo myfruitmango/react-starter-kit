df941e8ef00c4af49d0b5ac731a1cc8c8028ab22
fruitmango <123440759+myfruitmango@users.noreply.github.com>
2024-07-01 07:10:14 +0700

    Delete src/pages directory

--- src/pages/home/index.jsx
+++
@@ -1,9 +0,0 @@
-import Template from './template'
-
-const type = process.env.REACT_APP_ENV
-const port = process.env.PORT
-const HomePage = () => {
-	return <Template type={type} port={port} />
-}
-
-export default HomePage
