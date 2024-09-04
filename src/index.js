fcf27e2f48e09de02cde07b77e632708dd7d0d15
fruitmango <ctrl.fruitman90@gmail.com>
2024-09-04 07:21:52 +0700

    feat(index): add new import redux & store in src

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
+// ** React Imports
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
-import './index.css'
+// ** Store Imports
+import { store } from './config/store'
+
+// ** App Imports
 import App from './App'
+
+// ** Styles Imports
+import './index.css'
+
+// ** Others Imports
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-	<Router>
-		<App />
-	</Router>
+	<Provider store={store}>
+		<Router>
+			<App />
+		</Router>
+	</Provider>
 )
 
 reportWebVitals()
