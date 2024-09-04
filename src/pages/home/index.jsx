1fefe0a0d2318c22c89ea4ceab34dd756be90541
fruitmango <ctrl.fruitman90@gmail.com>
2024-09-04 07:24:54 +0700

    feat(pages): create new example pages home

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,15 @@
+// ** React Imports
+import React from 'react'
+
+// ** Frame Imports
+import Frame from 'components/frame/home'
+
+const HomePage = () => {
+	return (
+		<section className="w-screen h-screen flex justify-center items-center bg-slate-100">
+			<Frame />
+		</section>
+	)
+}
+
+export default HomePage
