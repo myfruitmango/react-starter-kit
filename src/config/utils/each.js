5bbe2b64f9be6b9832f35f2e4a55df7227085b24
fruitmango <123440759+myfruitmango@users.noreply.github.com>
2024-10-15 10:44:11 +0700

    Update each.js
    how to use utils

--- src/config/utils/each.js
+++ src/config/utils/each.js
@@ -4,3 +4,8 @@ import { Children } from 'react'
 export const Each = ({ render, of }) => {
 	return <>{Children.toArray(of.map((item, index) => render(item, index)))}</>
 }
+
+/**
+ * <Each of={categoryItems} render={item => ....} />
+ */
+
